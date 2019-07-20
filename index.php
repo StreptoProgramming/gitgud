41c5a16b9c8b04bd7b624399ebd4721991e1aff4
nsyawali12 <nsyawali12@gmail.com>
2019-07-20 13:26:56 +0700

    Data Kelas Sidebar

--- index.php
+++ index.php
@@ -1,42 +1,40 @@
 <!DOCTYPE html>
 <html>
 
-	<head>
-		<title> Absensi_SMP_Assalaam </title>
- 		<meta charset="utf-8">
- 		<meta name="viewport" content="width=device-width, initial-scale=1">		
-		<!-- CSS Bootstrap -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<head>
+    <title> Absensi_SMP_Assalam </title>
 
-		<!-- CSS Pure -->
-		<link rel="stylesheet" type="text/css" href="css/loginform.css">
-	</head>
+    <!-- CSS Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-	<body>
+    <!-- CSS Pure -->
+    <link rel="stylesheet" type="text/css" href="css/loginform.css">
 
-		<!-- Bagian Background -->
-		<section>
-			
+</head>
 
-			<div class="login-box">
-				<h1>Login Heres</h1>
-					<form method="post" action='loginasalam.php'>
-						<p>Username </p>
-						<div class="form-group">
-						<input type="text" name="username" placeholder="Enter your Email"></div>
-						<p> Password </p>
-						<div class="form-group"><input type="password" name="password" placeholder="Enter your Password"></div>
-						<button type="submit" name="submit" value="Login" class="btn btn-primary"> Submit </button>
-						<h4>Powered by <img src="./css/images/Logo-Putih.png" alt="" height="36px;" width="90px" style="margin-left: 5px;"></h4>						
-					</form>
-			</div>
-			
-		</section>
+<body>
+    <section>
+        <div class="login-box">
+            <h1> Login Here </h1>
+            <div>
+                <input type="button" name="button_1" value="Login Siswa" onClick="rdr('<?php echo "./view_loginsiswa.php"; ?>')">
+                <input type="button" name="button_2" value="Login Wali Kelas" onClick="rdr('<?php echo "./view_loginwali.php"; ?>')">
+                <input type="button" name="button_3" value="Login Piket" onClick="rdr('<?php echo "./view_loginpiket.php"; ?>')">
+                <input type="button" name="button_1" value="Login Admin" onClick="rdr('<?php echo "./view_loginadmin.php"; ?>')">
 
+            </div>
+        </div>
+    </section>
 
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-	</body>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script>
+        function rdr($lalal){
+            console.log($lalal);
+            window.location.href = $lalal;
+        }
+    </script>
+</body>
 
 </html>
